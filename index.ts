5c2e17988a02b8d32213c02f163d9123d92b6612
szaszfaijulia <szaszfai.julia@gmail.com>
2020-11-17 15:09:37 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -11,7 +11,8 @@ console.log(cntHasStarShip());
 
 //Add a vehiclesCount property to each object, the value of the property is number of items of the array of vehicles property
 function addVehiclesCount() {
-  people.forEach(person => person["vehiclesCount"] = person.vehicles.length);
+  //let perrson = {vehiclesCount: 0};
+  people.forEach(person => (person["vehiclesCount"] = person.vehicles.length));
 }
 
 addVehiclesCount();
@@ -21,7 +22,9 @@ people.forEach(p => console.log(p.name, p.vehiclesCount));
 
 //Sort the people object by the number of items in the array of films property
 //use the Array.prototype.sort method
-function sortByFilmsCnt() {}
+function sortByFilmsCnt() {
+  people.sort((p1, p2) => p2.films.length - p1.films.length);
+}
 console.log(" ");
 console.log("Sorted by films:");
 console.log("----------------");
@@ -30,7 +33,13 @@ people.forEach(p => console.log(p.name, p.films.length));
 
 //Sort the people objects by the number of items in the starships array and within that by the number of items in the vehicles array
 //use the Array.prototype.sort method
-function sortByStarshipsandVehicles() {}
+function sortByStarshipsandVehicles() {
+  people.sort(
+    (p1, p2) =>
+      p2.starships.length - p1.starships.length ||
+      p2.vehiclesCount - p1.vehiclesCount
+  );
+}
 console.log(" ");
 console.log("Sorted by starships, vehicles:");
 console.log("----------------");
@@ -39,9 +48,16 @@ people.forEach(p => console.log(p.name, p.starships.length, p.vehiclesCount));
 
 //Count the total number of vehicles
 //use the Array.prototype.reduce method
-function countVehicles() {}
+function countVehicles() {
+  const reducer = (accumulator, current) => accumulator + current.vehiclesCount;
+  return people.reduce(reducer, 0);
+}
 
 console.log(" ");
 console.log("Total vehicles count:");
 console.log("---------------------");
 console.log(countVehicles());
+
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
